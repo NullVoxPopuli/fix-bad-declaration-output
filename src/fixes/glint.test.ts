95ef46f2cb7b314de71fd2900b0db5e6648e9880
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-02-01 15:43:41 -0500

    Add tests

---
+++ src/fixes/glint.test.ts
@@ -0,0 +1,45 @@
+import { stripIndent } from 'common-tags';
+import { describe, expect as hardAssert, test } from 'vitest';
+
+import { fixGTSExtensions, fixOwnReferences } from './glint.js';
+
+const expect = hardAssert.soft;
+
+describe('fixGTSExtensions', () => {
+  test('works', () => {
+    let code = stripIndent`
+      import x from './foo.gts';
+    `;
+
+    let result = fixGTSExtensions(code);
+
+    expect(result).toBe(`import x from "./foo";`);
+  });
+});
+
+describe('fixOwnReferences', () => {
+  test('removes multiple', () => {
+    let code = stripIndent`
+      /// <reference types="node_modules/@glint/whatever/module">"
+      /// <reference types="node_modules/@glint/whatever2/module">"
+      /// <reference types="node_modules/@glint/whatever5/module">"
+      export const two = 2;`;
+
+    let result = fixOwnReferences(code);
+
+    expect(result).toBe(`export const two = 2;`);
+  });
+
+  test(' does not remove more than what is specified', () => {
+    let code = stripIndent`
+      /// <reference types="@glint/whatever/module">"
+      /// <reference types="node_modules/@glint/whatever2/module">"
+      export const two = 2;`;
+
+    let result = fixOwnReferences(code);
+
+    expect(result).toBe(stripIndent`
+      /// <reference types="@glint/whatever/module">"
+      export const two = 2;`);
+  });
+});
