b7b75579c416492a2147fc01a0eed8b8901477ac
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-08-22 12:11:29 -0400

    ope

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,19 @@
-import { configs } from '@nullvoxpopuli/eslint-configs';
+import { configs } from "@nullvoxpopuli/eslint-configs";
 
 const config = configs.node(import.meta.dirname);
 
 export default [
   ...config,
-  // your modifications here
-  // see: https://eslint.org/docs/user-guide/configuring/configuration-files#how-do-overrides-work
   {
-    files: ['**/*.test.ts'],
+    files: ["**/*.ts"],
     rules: {
-      'n/no-unpublished-import': 'off',
+      "n/no-missing-import": "off",
+    },
+  },
+  {
+    files: ["**/*.test.ts"],
+    rules: {
+      "n/no-unpublished-import": "off",
     },
   },
 ];
