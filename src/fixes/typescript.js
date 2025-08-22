112cb2fb8a0eb0cabf74e38fbdd1178145ac8025
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-08-22 13:18:21 -0400

    Fix for all declaration typse, particularly, non-declare

--- src/fixes/typescript.js
+++ src/fixes/typescript.js
@@ -1,6 +1,65 @@
+/**
+ * See: original code for withParser('ts')
+ *
+ * https://github.com/facebook/jscodeshift/blob/e877ed287dcb3ef3ee8b20c53ec8b477ab564438/parser/ts.js
+ */
+import babylon from '@babel/parser';
 import jscodeshift from 'jscodeshift';
 
-const j = jscodeshift.withParser('ts');
+function customTSParser() {
+  /**
+   * https://github.com/facebook/jscodeshift/blob/e877ed287dcb3ef3ee8b20c53ec8b477ab564438/parser/tsOptions.js
+   */
+  const options = {
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    allowReturnOutsideFunction: true,
+    startLine: 1,
+    tokens: true,
+    plugins: [
+      'asyncGenerators',
+      'decoratorAutoAccessors',
+      'bigInt',
+      'classPrivateMethods',
+      'classPrivateProperties',
+      'classProperties',
+      'decorators-legacy',
+      'doExpressions',
+      'dynamicImport',
+      'exportDefaultFrom',
+      'exportExtensions',
+      'exportNamespaceFrom',
+      'functionBind',
+      'functionSent',
+      'importAttributes',
+      'importMeta',
+      'nullishCoalescingOperator',
+      'numericSeparator',
+      'objectRestSpread',
+      'optionalCatchBinding',
+      'optionalChaining',
+      ['pipelineOperator', { proposal: 'minimal' }],
+      'throwExpressions',
+      /**
+       * Without dts: true, we don't properly parse all types of declaration files
+       *
+       * This is missing from the official tsOptions in jscodeshift
+       */
+      ['typescript', { dts: true }],
+    ],
+  };
+
+  return {
+    /**
+     * @param {string} code
+     */
+    parse(code) {
+      return babylon.parse(code, options);
+    },
+  };
+}
+
+const j = jscodeshift.withParser(customTSParser());
 
 /**
  * @param {string} contents
